74b7cf05afae2e9d28bf3f6619c3cdf23178d961
app_le_com <app_le_com@192.168.1.11>
2022-07-11 11:31:36 +0530

    adding test folders

--- divide_train_test_set.py
+++ divide_train_test_set.py
@@ -1,19 +1,35 @@
 import os
 import shutil
-import sys
+#import sys
 
-datas_path = sys.argv[1]
+#datas_path = sys.argv[1]
+tests = [
+'football/',
+'biker/', 
+'bird1/', 
+'blurbody/', 
+'blurcar2/', 
+'bolt/', 
+'cardark/',
+'human3/',
+'human6/',
+'human9/',
+'panda/',
+'walking/',
+'walking2/']
 
 #print(datas_path)
-if(not os.path.exists(os.path.join(datas_path, 'test'))):
-    os.mkdir(datas_path + '/test')
-
-files_p = os.listdir(datas_path + '/p')
-for (i,f) in enumerate(files_p):
-    if((i % 7) == 0):
-        if(i == 0):
-            files_p_test = [f]
-        else:
-            files_p_test.append(f)
-        shutil.move(os.path.join(datas_path + '/p', f), os.path.join(datas_path, 'test'))
+for datas_path in tests:
+    datas_path += '/' + datas_path +'/'
+    if(not os.path.exists(os.path.join(datas_path, 'test'))):
+        os.mkdir(datas_path + '/test')
+    
+    files_p = os.listdir(datas_path + '/p')
+    for (i,f) in enumerate(files_p):
+        if((i % 7) == 0):
+            if(i == 0):
+                files_p_test = [f]
+            else:
+                files_p_test.append(f)
+            shutil.move(os.path.join(datas_path + '/p', f), os.path.join(datas_path, 'test'))
 
