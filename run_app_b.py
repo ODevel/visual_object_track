69ff05148f430db2dbf8034397974592a523b3a2
app_le_com <app_le_com@app-le-coms-MacBook-Pro.local>
2022-07-26 13:18:06 +0530

    combining app a and b in app b + adding vid

--- run_app_b.py
+++ run_app_b.py
@@ -4,44 +4,55 @@ import numpy as np
 import os
 import sys
 
-def run(test):
+def path_to_image(path):
+    img = cv2.imread(path)
+    l = img.shape[0]
+    b = img.shape[1]
+    lf = 1
+    bf = 1
+    # TF Opt.
+    if(img.shape[0] > 128 and img.shape[1] > 96):
+        l = 128
+        b = 96
+        lf = 128/img.shape[0]
+        bf = 96/img.shape[1]
+    elif(img.shape[0] > 128):
+        l = 128
+        lf = 128/img.shape[0]
+    elif(img.shape[1] > 96):
+        b = 96
+        bf = 96/img.shape[1]
+
+    img = cv2.resize(img, (b,l))
+    img = tf.expand_dims(img,0)
+    return img, (lf, bf)
+
+def run(image_path):
+    cnn_detection = tf.keras.models.load_model('simple_detection.h5')
+    clsses = ['biker', 'bird1', 'blurbody', 'blurcar2', 'bolt', 'cardark', 'football', 'human3', 'human6', 'human9', 'panda', 'walking', 'walking2']
+    img_orig = cv2.imread(image_path)
+    img, (lf, bf) = path_to_image(image_path)
+    cls = cnn_detection.predict(img)
+    idx = np.argmax(cls)
+    test = clsses[idx]
+    print('Class predicted: ', test)
     cnn00 = tf.keras.models.load_model(test +'_00.h5')
     cnn01 = tf.keras.models.load_model(test +'_01.h5')
     cnn10 = tf.keras.models.load_model(test +'_10.h5')
     cnn11 = tf.keras.models.load_model(test +'_11.h5')
     
     #test = 'human9'
-    test_p = test +'/' + test +'/'
-    files = sorted(os.listdir(test_p +'/p/'))
-    i = 0
-    for f in files:
-        f = test_p + '/p/' + f
-        img = cv2.imread(f)
-        img_orig= img
-        bf = 1
-        lf = 1
-        if(img.shape[1] > 128 and img.shape[0] > 96):
-            img = cv2.resize(img, (96,128))
-            bf = (img_orig.shape[0]/96)
-            lf = (img_orig.shape[1]/128)
-        elif (img.shape[0] > 96):
-            img = cv2.resize(img, (96, img.shape[1]))
-            bf = (img_orig.shape[0]/96)
-        elif(img.shape[1] > 128):
-            img = cv2.resize(img, (img.shape[0], 128))
-            lf = (img_orig.shape[1]/128)
-        x = cnn00.predict(tf.expand_dims(img, 0))
-        y = cnn01.predict(tf.expand_dims(img, 0))
-        w = cnn10.predict(tf.expand_dims(img, 0))
-        h = cnn11.predict(tf.expand_dims(img, 0))
+    x = cnn00.predict(img)
+    y = cnn01.predict(img)
+    w = cnn10.predict(img)
+    h = cnn11.predict(img)
     
-        x *= bf
-        y *= lf
-        w *= bf
-        h *= lf
-        print('x:', x, 'y: ', y, img_orig.shape)
-        cv2.putText(img_orig, test, (int(x+5),int(y-5)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-        cv2.rectangle(img_orig, (int(x),int(y)),(int((x+w)),int((y+h))), (0,255,0), 2)
-        cv2.imwrite('cam' +str(i) +'.jpg', img_orig)
-        cv2.imshow('cam' +str(i) +'.jpg', img_orig)
-        i+=1
+    x /= bf
+    y /= lf
+    w /= bf
+    h /= lf
+    print('x:', x, 'y: ', y, img_orig.shape)
+    cv2.putText(img_orig, test, (int(x+5),int(y-5)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+    cv2.rectangle(img_orig, (int(x),int(y)),(int((x+w)),int((y+h))), (0,255,0), 2)
+    cv2.imwrite('cam' +'001' +'.jpg', img_orig)
+    cv2.imshow('cam' +'001' +'.jpg', img_orig)
---
+++ run_app_b_vid.py
@@ -0,0 +1,49 @@
+import run_app_b
+import cv2
+import os
+import tensorflow as tf
+import numpy as np
+
+def run(test):
+    cnn_detection = tf.keras.models.load_model('simple_detection.h5')
+    clsses = ['biker', 'bird1', 'blurbody', 'blurcar2', 'bolt', 'cardark', 'football', 'human3', 'human6', 'human9', 'panda', 'walking', 'walking2']
+    test_p = test +'/' + test +'/'
+    files = sorted(os.listdir(test_p +'/p/'))
+    model_loaded = {}
+    models = {}
+    for each_cls in clsses:
+        model_loaded[each_cls] = False
+
+    for (i,f) in enumerate(files):
+        f = test_p + '/p/' + f
+        img_orig = cv2.imread(f)
+        img, (lf, bf) = run_app_b.path_to_image(f)
+        cls = cnn_detection.predict(img)
+        idx = np.argmax(cls)
+        test = clsses[idx]
+        print('Class predicted: ', test)
+        if(model_loaded[test] == False):
+            models[test] = [tf.keras.models.load_model(test +'_00.h5')]
+            models[test].append(tf.keras.models.load_model(test +'_01.h5'))
+            models[test].append(tf.keras.models.load_model(test +'_10.h5'))
+            models[test].append(tf.keras.models.load_model(test +'_11.h5'))
+            model_loaded[test] = True
+        cnn00 = models[test][0]
+        cnn01 = models[test][1]
+        cnn10 = models[test][2]
+        cnn11 = models[test][3]
+        x = cnn00.predict(img)
+        y = cnn01.predict(img)
+        w = cnn10.predict(img)
+        h = cnn11.predict(img)
+    
+        x /= bf
+        y /= lf
+        w /= bf
+        h /= lf
+        print('x:', x, 'y: ', y, img_orig.shape)
+        cv2.putText(img_orig, test, (int(x+5),int(y-5)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+        cv2.rectangle(img_orig, (int(x),int(y)),(int((x+w)),int((y+h))), (0,255,0), 2)
+        #cv2.imwrite('cam' +str(i) +'.jpg', img_orig)
+        cv2.imshow('cam' +str(i) +'.jpg', img_orig)
+        i+=1 
